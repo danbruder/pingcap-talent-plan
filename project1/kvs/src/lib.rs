167c3db207105185515c4a5aa27fbc4181b52745
Dan Bruder <danb@structionsite.com>
2020-12-09 20:42:05 -0500

    Structure

--- project1/kvs/src/lib.rs
+++ project1/kvs/src/lib.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        panic!("atd");
+    }
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!("atd");
+    }
+    pub fn set(&self, key: String, value: String) {
+        panic!("atd");
+    }
+    pub fn remove(&self, key: String) {
+        panic!("atd");
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
