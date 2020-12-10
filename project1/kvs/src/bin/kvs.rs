0d3f825f5c6cd94083368e50c27a23cb3ab53e86
Dan Bruder <danb@structionsite.com>
2020-12-09 21:26:26 -0500

    commands are passing

--- project1/kvs/src/bin/kvs.rs
+++ project1/kvs/src/bin/kvs.rs
@@ -4,40 +4,29 @@ use clap::{App, Arg, SubCommand};
 fn main() {
     let matches = App::new("kvs")
         .version("1.0")
-        // .arg(
-        //     Arg::with_name("config")
-        //         .short("c")
-        //         .long("config")
-        //         .value_name("FILE")
-        //         .help("Sets a custom config file")
-        //         .takes_value(true),
-        // )
-        // .arg(
-        //     Arg::with_name("INPUT")
-        //         .help("Sets the input file to use")
-        //         .required(true)
-        //         .index(1),
-        // )
-        // .arg(
-        //     Arg::with_name("v")
-        //         .short("v")
-        //         .multiple(true)
-        //         .help("Sets the level of verbosity"),
-        // )
-        // .subcommand(
-        //     SubCommand::with_name("test")
-        //         .about("controls testing features")
-        //         .version("1.3")
-        //         .author("Someone E. <someone_else@other.com>")
-        //         .arg(
-        //             Arg::with_name("debug")
-        //                 .short("d")
-        //                 .help("print debug information verbosely"),
-        //         ),
-        // )
+        .arg(Arg::with_name("V").short("V"))
+        .subcommand(SubCommand::with_name("get").arg(Arg::with_name("KEY").required(true).index(1)))
+        .subcommand(
+            SubCommand::with_name("set")
+                .arg(Arg::with_name("KEY").required(true).index(1))
+                .arg(Arg::with_name("VALUE").required(true).index(2)),
+        )
+        .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("KEY").required(true).index(1)))
         .get_matches();
 
-    // more program logic goes here...
-
-    std::process::exit(1);
+    if matches.occurrences_of("V") > 0 {
+        println!("0.1.0");
+        std::process::exit(0);
+    } else if let Some(_) = matches.subcommand_matches("get") {
+        eprintln!("unimplemented");
+        std::process::exit(1);
+    } else if let Some(_) = matches.subcommand_matches("rm") {
+        eprintln!("unimplemented");
+        std::process::exit(1);
+    } else if let Some(_) = matches.subcommand_matches("set") {
+        eprintln!("unimplemented");
+        std::process::exit(1);
+    } else {
+        std::process::exit(1);
+    }
 }
